de8ab288ee2369d994060b66607cd7b66299decf
Alikhan Oitan <alikhan.oitan@gmail.com>
2024-10-07 13:03:28 +0500

    add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
       forbidUnknownValues: true,
+      transform: true,
     }),
   );
+
+  bootstrapDocumentation(app);
+
   await app.listen(3000);
 }
+
+function bootstrapDocumentation(app: INestApplication) {
+  const theme = new SwaggerTheme();
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest App')
+    .setDescription('Nest App API')
+    .setVersion('1.0')
+    .addTag('app')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    jsonDocumentUrl: 'api/json',
+    yamlDocumentUrl: 'api/yaml',
+    explorer: true,
+    customCss: theme.getBuffer(SwaggerThemeNameEnum.DRACULA),
+  });
+}
+
 bootstrap();
