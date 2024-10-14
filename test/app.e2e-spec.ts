e1cb2d421464093dd12ede99509767e9994b9e7a
Alikhan Oitan <alikhan.oitan@gmail.com>
2024-10-14 20:23:18 +0500

    remove unit tests and fix e2e open handles

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,9 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { DataSource } from 'typeorm';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let connection: DataSource;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -12,10 +14,15 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    connection = moduleFixture.get<DataSource>(DataSource);
     await app.init();
   });
 
   it('/ (GET)', () => {
     return request(app.getHttpServer()).get('/').expect(200).expect('pong');
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
   });
 });
